152f0bf12ea001fe9f5e6b39dcd93b8674f8ccb9
M. Vondano <m-vo@users.noreply.github.com>
2023-04-07 12:56:44 +0200

    CS

--- src/Component/ComponentManager.php
+++ src/Component/ComponentManager.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/*
+ * @copyright eBlick Medienberatung
+ * @license   LGPL-3.0+
+ * @link      https://github.com/eBlick/contao-trigger
+ */
+
 namespace EBlick\ContaoTrigger\Component;
 
 use EBlick\ContaoTrigger\Component\Action\ActionInterface;
--- src/EventListener/DataContainer/ExecutionLog.php
+++ src/EventListener/DataContainer/ExecutionLog.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 /*
  * @copyright eBlick Medienberatung
- * @license LGPLv3
+ * @license   LGPL-3.0+
+ * @link      https://github.com/eBlick/contao-trigger
  */
 
 namespace EBlick\ContaoTrigger\EventListener\DataContainer;
